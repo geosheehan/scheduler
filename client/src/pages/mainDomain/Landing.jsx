4ed527e4df0d91e2d6b48d5f566dd06cbcd83f0d
medium0fry <medium0fry@gmail.com>
2021-07-27 10:50:08 -0400

    Material UI-ified the landing page

--- client/src/pages/mainDomain/Landing.jsx
+++ client/src/pages/mainDomain/Landing.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter as Router, Link, Redirect } from 'react-router-dom';
+import { Button, Box, Container } from '@material-ui/core';
+
 import Navigation from '../../components/navigation/Navigation';
 
 const Landing = () => {
 	return (
 		<>
 			<Navigation />
-			<main>
-				<h1>Welcom to FoxFry</h1>
-				<span>Here I go convincing you that you need this site.</span>
-				<button onClick={() => <Redirect to="/creators" />}>
-					Find Creators
-				</button>
-				<button onClick={() => <Redirect to="/signup" />}>Sign Up</button>
-			</main>
+			<Container component="main">
+				<Box>
+					<h1>Welcome to FoxFry</h1>
+					<span>Here I go convincing you that you need this site.</span>
+					<Button variant="contained">Find Creators</Button>
+					<Button variant="contained" color="primary">
+						Sign Up
+					</Button>
+				</Box>
+			</Container>
 		</>
 	);
 };
